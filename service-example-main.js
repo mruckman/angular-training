bf1e08dfb63add0e6f51672566f38d6c7e089789
Maurice Ruckman <nospam2008@mruckman.com>
2013-12-01 11:31:03 -0800

    Created a filter

--- service-example-main.js
+++ service-example-main.js
@@ -4,7 +4,19 @@ myApp.factory('Data', function() {
 	return {message:"I'm data from a service"}
 })
 
+myApp.filter('titleCase', function(Data) {
+	var titleCaseFilter = function(input) {
+		var words = input.split(' ');
+		for (var i = 0; i < words.length; i++) {
+			words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+		}
+		return words.join(' ') + " and " + Data.message;
+	};
+	return titleCaseFilter;
+});
+
 function FirstCtrl($scope, Data) {
+	$scope.pageHeading = 'behold the majesty of your page title';
 	$scope.data = Data;
 }
 
